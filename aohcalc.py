c2bdcfadd462791fe757bfa31bb2201e33bc224b
Omar Tanner <pixzlert@gmail.com>
2025-10-11 19:44:57 +0100

    .

--- aohcalc.py
+++ aohcalc.py
@@ -55,7 +55,7 @@ def load_species_info(path: Path):
             return pd.DataFrame([feat["properties"] for feat in src])
     except Exception as e:
         # Fallback to geopandas if Fiona fails
-        print("failed to use fiona")
+        print(f"failed to use fiona {e}")
         import geopandas as gpd
         return gpd.read_file(path, ignore_geometry=True)
 
